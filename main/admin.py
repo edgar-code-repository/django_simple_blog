a4eb4bae8379061aac1fad145b9e0972202a89c0
ECD <ecofre@gmail.com>
2019-11-17 19:21:01 -0300

    Se agregan models y se configuran para ser manipulados desde la aplicacion admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Technology, Category, Post
+
+
+class TechnologyModelAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+    class Meta:
+        model = Technology
+
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "technology"]
+
+    class Meta:
+        model = Category
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "publication_date", "last_modification_date"]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Technology, TechnologyModelAdmin)
+admin.site.register(Category, CategoryModelAdmin)
+admin.site.register(Post, PostModelAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -12,3 +13,39 @@ class AppUser(AbstractUser):
         return "[User: " + self.username + " - Email: " + self.email + "]"
 
 
+class Technology(models.Model):
+    name = models.CharField(max_length=150)
+
+    class Meta:
+        ordering = ["name"]
+    
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    technology = models.ForeignKey(Technology, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    image = models.ImageField()
+
+    class Meta:
+        ordering = ["name"]
+    
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    summary = models.CharField(max_length=250)
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    last_modification_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        ordering = ["-last_modification_date", "-publication_date"]
+    
+    def __str__(self):
+        return self.title
