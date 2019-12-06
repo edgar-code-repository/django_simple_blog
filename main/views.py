edf773759e79d5a451b7fd8fb24cf1b76d9cb6c1
ECD <ecofre@gmail.com>
2019-12-05 22:43:20 -0300

    Se agrega despliegue de detalle del post

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -27,3 +27,19 @@ class PostList(ListView):
         print("[PostList][paginator_page: " + str(paginator_page.object_list) + "]")
 
         return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = "main/post_detail.html"
+    context_object_name = 'post'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostDetail, self).get_context_data(*args, **kwargs) 
+
+        page = self.request.GET.get('page')
+        print("[PostDetail][Page: " + str(page) + "]")
+        context['page'] = page
+
+        return context
+
